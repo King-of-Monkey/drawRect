0e274aa94376a8f48ac4c1d498d6cf6f302e42ac
wkaiyan@163.com <wkaiyan@163.com>
2016-03-29 09:00:22 +0800

    Initial Commit

---
+++ DrawRect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawRect
+//
+//  Created by WKY on 16/3/29.
+//  Copyright © 2016年 WKY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
